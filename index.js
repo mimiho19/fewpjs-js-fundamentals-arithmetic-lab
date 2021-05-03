d3ee83722c6aad2f62544998d3425a720489f889
Mimi Ho <somymimiho@gmail.com>
2021-05-03 18:14:28 -0500

    commit tree fa755ae903288e0fcc1db38d66bf5d0f6e4e6b9c

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; 
+// the result of the multiplication should be 62.
+
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+const max = Math.max( 5, 10, 15, 20 );
